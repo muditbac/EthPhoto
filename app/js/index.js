f86cd24ecb16d7ac79106b854c3dd6b05b8600a2
shubhamwaghe <shubhamwaghe@gmail.com>
2017-03-18 18:15:10 +0530

    Image file upload edit

---
+++ app/js/index.js
@@ -0,0 +1,34 @@
+// Function to get final Edited Image
+function getEditedImage() {
+  var newImage = board.canvas.toDataURL();
+  // fileStorageLocation = newImage;
+  // imageObj = newImage;
+  var blob = dataURItoBlob(newImage);
+  console.log(blob);
+  return blob;
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    //Old Code
+    //write the ArrayBuffer to a blob, and you're done
+    //var bb = new BlobBuilder();
+    //bb.append(ab);
+    //return bb.getBlob(mimeString);
+
+    //New Code
+    return new Blob([ab], {type: mimeString});
+}
